0f0ee2fe723fb0010ed4007db18e13735cd266a5
Peter Wen <peter@whitehalmos.org>
2014-02-08 13:05:42 -0500

    v0.1.3

--- format.js
+++ format.js
@@ -6,7 +6,11 @@
 	
 	// Duplicate territory name.
 	var smallMap = document.getElementsByClassName("overview")[0];
-	var title = smallMap.previousSibling.previousSibling;
+	var title = smallMap.previousSibling;
+	if (title.tagName != 'H1') {
+		// Accomodate optional territory "Notes".
+		title = title.previousSibling;
+	}
 	var bigMap = title.previousSibling;
 	var title2 = title.cloneNode(true);
 	title2.children[1].innerText += ' Map';

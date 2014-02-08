816171c3ca96a5f1da94fed03e3c102e243ee8a3
Peter Wen <peter@whitehalmos.org>
2014-02-08 12:48:28 -0500

    v0.1.2

--- format.js
+++ format.js
@@ -1,13 +1,13 @@
 (function() {
+	// Reorder smaller map so it doesn't take up extra space
 	var smallMap = document.getElementsByClassName("overview")[0];
 	var pSubtitle = smallMap.previousSibling.previousSibling;
-	var title = pSubtitle.previousSibling;
-	var bigMap = title.previousSibling;
-	
-	// Reorder smaller map so it doesn't take up extra space
 	smallMap.parentNode.insertBefore(smallMap, pSubtitle);
 	
 	// Duplicate territory name
+	var smallMap = document.getElementsByClassName("overview")[0];
+	var title = smallMap.previousSibling.previousSibling;
+	var bigMap = title.previousSibling;
 	var title2 = title.cloneNode(true);
 	title2.children[1].innerText += ' Map';
 	title.parentNode.insertBefore(title2, bigMap);
@@ -20,6 +20,19 @@
 	  if (ths[i].innerText === "NAME & TELEPHONE") {
 		ths[i].innerText = "TELEPHONE";
 		break;
+	  }
+	}
+	
+	// Do not display names
+	var addressTable = document.getElementsByClassName("addresses")[0];
+	var tbody = addressTable.getElementsByTagName('tbody')[0];
+	var trs = tbody.getElementsByTagName('tr');
+	for (var i = 0; i < trs.length; ++i) {
+	  if (trs[i].children.length >= 4) {
+		var td = trs[i].children[3];
+		if (td.children.length >= 2 && td.children[0].tagName == 'STRONG') {
+			td.removeChild(td.children[0]);
+		}
 	  }
 	}
 })();

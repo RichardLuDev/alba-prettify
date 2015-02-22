c6c91d603206b4d6295e4d911d1ca8eea3d5e404
Peter Wen <peter@whitehalmos.org>
2015-02-22 13:24:33 -0500

    Fix duplicate download requests.
    Add analytics.

--- src/addresses.js
+++ src/addresses.js
@@ -1,4 +1,6 @@
+var ADDRESSES = 'addresses';
 var SAVE_ID = 'alba-prettify-export-save';
+
 document.querySelector('.cmd-export').addEventListener('click', function() {
 	var saveElement = document.getElementById(SAVE_ID);
 	if (saveElement === null) {
@@ -9,23 +11,28 @@ document.querySelector('.cmd-export').addEventListener('click', function() {
 		saveElement.appendChild(saveText);
 		this.parentNode.insertBefore(saveElement, this);
 		saveElement.disabled = true;
+		
+		saveElement.addEventListener('click', function(e) {
+			// Download the textarea as file.
+			var text = document.querySelector('#export').textContent;
+			var blob = new Blob([text], {type: 'text/plain'});
+			var date = new Date();
+			var name = (
+					date.toISOString().replace(/T.*$/, '') + ' ' +
+					'Alba Backup.txt');
+			var link = document.createElement('a');
+			link.download = name;
+			link.href = window.URL.createObjectURL(blob);
+			link.click();
+			// Prevent page from reloading.
+			e.preventDefault();
+			
+			Analytics.recordEvent(ADDRESSES, 'click', SAVE_ID);
+		});
 	}
+	
+	// Focus is the only event that reliably fires after pressing export.
 	document.querySelector('#export').addEventListener('focus', function() {
 		saveElement.disabled = false;
-	});
-	saveElement.addEventListener('click', function(e) {
-		// Download the textarea as file.
-		var text = document.querySelector('#export').textContent;
-		var blob = new Blob([text], {type: 'text/plain'});
-		var date = new Date();
-		var name = (
-				date.toISOString().replace(/T.*$/, '') + ' ' +
-				'Alba Backup.txt');
-		var link = document.createElement('a');
-		link.download = name;
-		link.href = window.URL.createObjectURL(blob);
-		link.click();
-		// Prevent page from reloading.
-		e.preventDefault();
 	});
 });
